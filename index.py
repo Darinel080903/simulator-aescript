ecc0ae4ffcbfc9ee4b3546c23a7583dea0f7b6bc
Darinel Monjaras Ruiz <darinelmr12345@gmail.com>
2023-11-05 12:45:00 -0600

    main added

--- index.py
+++ index.py
@@ -20,8 +20,8 @@ class IDESimulator:
             self.result_label.config(text="Código inválido")
 
     def is_valid_code(self, code):
-        # Expresión regular para validar la declaración de variables con números (positivos y negativos) hasta 1000000
-        variable_pattern = r"variable\s+[a-zA-Z]+\s*=\s*-?\d+;"
+        # Expresión regular para validar la declaración de variables con cualquier tipo de valor
+        variable_pattern = r"variable\s+[a-zA-Z]+\s*=\s*[\w\.-]+;"
         
         # Expresión regular para validar la estructura de la función y la palabra reservada "imprimir"
         function_pattern = r"funcion\s+[a-zA-Z]+\s*>>\s*{\s*[a-zA-Z]+\s*[\+\-\/*]\s*\d+\s*;\s*}\s*imprimir\(\s*[a-zA-Z]+\s*\);"
@@ -32,8 +32,11 @@ class IDESimulator:
         # Expresión regular para validar la estructura de la sentencia "si"
         si_pattern = r"si\s+[a-zA-Z]+\s*(>\s*|\s*<\s*)\d+\s*realiza\s*{\s*[a-zA-Z]+\s*[\+\-\/*]\s*\d+\s*;\s*}\s*"
         
-        # Validar la declaración de variables, estructura de la función, sentencia "iterar" y sentencia "si"
-        if re.match(variable_pattern, code) or re.match(function_pattern, code) or re.match(iterar_pattern, code) or re.match(si_pattern, code):
+        # Expresión regular para validar la estructura de la función "principal"
+        principal_pattern = r"funcion\s+[a-zA-Z]+\s+correr\s*>>\s*{\s*>>[a-zA-Z]+\s*;\s*}\s*"
+        
+        # Validar la declaración de variables, estructura de la función, sentencia "iterar", sentencia "si" y función "principal"
+        if re.match(variable_pattern, code) or re.match(function_pattern, code) or re.match(iterar_pattern, code) or re.match(si_pattern, code) or re.match(principal_pattern, code):
             return True
 
         return False
